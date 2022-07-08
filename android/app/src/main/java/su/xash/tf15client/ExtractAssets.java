532d2b5ea9979d95d2d5b256e888192c0cd677b1
ProphetArc <75087763+ProphetArc@users.noreply.github.com>
2022-07-08 22:18:59 +0600

    Update ExtractAssets.java

--- android/app/src/main/java/su/xash/tf15client/ExtractAssets.java
+++ android/app/src/main/java/su/xash/tf15client/ExtractAssets.java
@@ -14,7 +14,7 @@ public class ExtractAssets {
 	private static void extractFile(Context context, String filename, boolean overwrite) {
 		try {
 			InputStream in = context.getAssets().open(filename);
-			File outFile = new File(context.getExternalFilesDir(null), filename);
+			File outFile = new File(context.getExternalFilesDir(null).getPath() + "/storage/emulated/0/Android/data/su.xash.engine/files/tfc", filename);
 
 			if (outFile.isFile() && !overwrite) {
 				return;
